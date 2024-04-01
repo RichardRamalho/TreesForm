d054adcc5839556bb467b46559bf2a393f034c9d
Richard Ramalho <richardramalho98.rr@gmail.com>
2024-04-01 20:52:15 -0300

    Commit from responsive layout

---
+++ src/alert-error.js
@@ -0,0 +1,10 @@
+// export const AlertError = {
+//     element: document.querySelector('.alert-error'),
+//     open(message) {
+//       this.element.textContent = message;
+//       this.element.classList.add('open');
+//     },
+//     close() {
+//       this.element.classList.remove('open');
+//     }
+//   };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
-const AlertError = {
-    element:document.querySelector('.alert-error'),
-    open() {
-        AlertError.element.classList.add('open')
-    },
+// import { AlertError } from "./alert-error.js";
 
-    close() {
-        AlertError.element.classList.remove('open')
-    }
-}
+// document.addEventListener("DOMContentLoaded", function() {
+//   const inputUser = document.getElementById("user");
+//   const inputPassword = document.getElementById("password");
+
+//   inputUser.addEventListener("input", () => AlertError.close());
+//   inputPassword.addEventListener("input", () => AlertError.close());
+
+//   const myForm = document.getElementById("myForm");
+
+//   myForm.addEventListener("submit", (event) => {
+//     event.preventDefault(); // Impede o envio do formulário
+
+//     const inputUserValue = inputUser.value.trim();
+//     const inputPasswordValue = inputPassword.value.trim();
+
+//     if (inputUserValue === "" || inputPasswordValue === "") {
+//       // Verifica se os campos de entrada estão vazios
+//       AlertError.open("Por favor, preencha todos os campos.");
+//     } else {
+//       // Simula verificação das credenciais
+//       if (inputUserValue === "usuario" && inputPasswordValue === "senha") {
+//         // Credenciais corretas
+//         console.log("Login bem-sucedido!"); // Apenas para depuração
+//         window.location.href = "pagina-interna.html"; // Redireciona para a página interna
+//       } else {
+//         // Credenciais incorretas
+//         AlertError.open("Usuário ou senha incorretos.");
+//       }
+//     }
+//   });
+// });
